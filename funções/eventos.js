c239db93c6e5acce40371c4c4f1816c0caf06885
Isabella <miguelslm@hotmail.com>
2024-05-20 08:05:52 -0300

    Aula de eventos

---
+++ funções/eventos.js
@@ -0,0 +1,13 @@
+const apresentarLog = () => {
+    console.log(`Olá, Mundo!`);
+}
+
+const clickAction = () => {
+    console.log(`Você clicou!`);
+}
+
+const alertPage = () => {
+    console.clear();
+    alert("Bem-vindo!!");
+    console.log("Bem-vindo!!");
+}
--- funções/funcao.js
+++ funções/funcao.js
@@ -1,4 +1,4 @@
-function mediaAluno() {
+function testeLog() {
     console.log("Olá, Alunos!")
 }
 
@@ -7,4 +7,17 @@ function mediaAlunoIos(n1 = 0, n2 = 0) { // os parâmetros recebem 0
     return media; // precisa retornar um valor númerico
 }
 
-console.log(mediaAlunoIos()); // chama a função
+function apresentar() {
+    console.log(`Olá, Mundo!`);
+}
+
+// console.log(mediaAlunoIos(5, 10)); // chama a função
+apresentar();
+
+// Arrow Function
+const mediaAluno = (n1 = 0, n2 = 0, rep = 0) => {
+    let media = (n1 + n2 + rep) / 3;
+    return media;
+}
+
+console.log(mediaAluno(8, 6.6, 10).toFixed(1));
