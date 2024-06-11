5f5154e38bf4e673f0f0519125a5bf1f915ea1cf
César <cesar.eduar.oliveira>
2024-06-11 11:51:36 -0300

    Aula de regex

---
+++ atividades/atv-regex/regEx.js
@@ -0,0 +1,8 @@
+let botao = document.querySelector("#validacao");
+let email = document.querySelector("#email");
+let senha = document.querySelector("#senha");
+let celular = document.querySelector("#celular");
+
+botao.addEventListener("click", () => {
+    
+});
